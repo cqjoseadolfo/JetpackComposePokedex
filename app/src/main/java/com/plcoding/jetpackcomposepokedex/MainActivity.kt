9635c220e74368895db2045538f4175710fa3deb
root <root@DESKTOP-ANGIACE>
2023-11-16 22:08:32 -0500

    compose layouts

--- app/src/main/java/com/plcoding/jetpackcomposepokedex/MainActivity.kt
+++ app/src/main/java/com/plcoding/jetpackcomposepokedex/MainActivity.kt
@@ -17,6 +17,7 @@ import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.navArgument
 import androidx.navigation.compose.rememberNavController
+import com.plcoding.jetpackcomposepokedex.pokemonlist.PokemonListScreen
 import com.plcoding.jetpackcomposepokedex.ui.theme.JetpackComposePokedexTheme
 import dagger.hilt.android.AndroidEntryPoint
 
@@ -26,13 +27,13 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             JetpackComposePokedexTheme {
-                val navCotroller = rememberNavController()
+                val navController = rememberNavController()
                 NavHost(
-                    navController = navCotroller,
+                    navController = navController,
                     startDestination = "pokemon_list_screen"
                 ){
                     composable("pokemon_list_screen") {
-
+                        PokemonListScreen(navController = navController)
                     }
                     composable(
                         "pokemon_detail_screen/{dominantColor}/{pokemonName}",
